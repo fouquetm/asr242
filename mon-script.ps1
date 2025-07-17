4a178f571bcda28d7a7df74e73974b4e8a050cab
fouquetm <svplzirl@gmail.com>
2025-07-17 13:58:07 +0200

    ajout dummy function

--- mon-script.ps1
+++ mon-script.ps1
@@ -8,3 +8,11 @@ Write-Host "Param2: $param2"
 
 # Example usage:
 # .\mon-script.ps1 -param1 "Hello" -param2 42
+
+function ExampleFunction {
+    param (
+        [string]$inputString
+    )
+    
+    Write-Host "Input String: $inputString"
+}
