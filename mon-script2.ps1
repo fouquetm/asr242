a29d533aabbf3473e8447c5f1fab0f88787bf871
fouquetm <svplzirl@gmail.com>
2025-07-17 15:01:08 +0200

    duplicata script ps

---
+++ mon-script2.ps1
@@ -0,0 +1,42 @@
+param (
+    [string]$param1,
+    [int]$param2
+)
+
+Write-Host "Param1: $param1"
+Write-Host "Param2: $param2"
+
+# Example usage:
+# .\mon-script.ps1 -param1 "Hello" -param2 42
+
+function ExampleFunctionWithDefault {
+    param (
+        [string]$inputString = "Default Value"
+    )
+    
+    Write-Host "Input String: $inputString"
+}
+
+function ExampleFunction {
+    param (
+        [string]$inputBoolean = $true,
+        [int]$inputNumber = 10,
+        [string]$inputString = "Hello, World!"
+    )
+
+    Write-Host "Input Boolean: $inputBoolean"
+    Write-Host "Input Number: $inputNumber"
+    Write-Host "Input String: $inputString"
+}
+
+function ExampleFunctionWithSwitch {
+    param (
+        [switch]$verbose
+    )
+    
+    if ($verbose) {
+        Write-Host "Verbose mode is ON"
+    } else {
+        Write-Host "Verbose mode is OFF"
+    }
+}
